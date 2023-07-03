55745785c18b8205dcf399c2635d9c4ebc8d7da9
pureliture <tkdgur1756@naver.com>
2023-07-03 16:55:09 +0900

    :sparkles: 매핑테이블 DDL 추가

--- src/main/resources/db/migration/ddl/V0_0_3__create_table_hashtag.sql
+++ src/main/resources/db/migration/ddl/V0_0_3__create_table_hashtag.sql
@@ -1,5 +1,5 @@
 CREATE TABLE hashtag (
   id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '해시태그아이디',
   name VARCHAR(255) NOT NULL COMMENT '해시태그이름',
-  book_id BIGINT COMMENT '책아이디'
+  category_id BIGINT COMMENT '카테고리아이디'
 ) COMMENT '해시태그';
