72bc621b33e471d74b28f9a139daef336a8f014c
pureliture <tkdgur1756@naver.com>
2023-07-05 09:33:37 +0900

    :sparkles: 매핑테이블 DDL 추가

--- src/main/resources/db/migration/ddl/V0_0_4__create_table_category_book_mapping.sql
+++ src/main/resources/db/migration/ddl/V0_0_4__create_table_category_book_mapping.sql
@@ -1,5 +1,5 @@
 CREATE TABLE category_book_mapping (
   id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '분류도서매핑아이디',
   category_id BIGINT COMMENT '카테고리아이디',
-  book_id BIGINT COMMENT '도서',
+  book_id BIGINT COMMENT '도서'
 ) COMMENT '분류도서매핑';
