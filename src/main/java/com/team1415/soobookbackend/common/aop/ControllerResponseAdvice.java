4a67d9b1d56d8aa5dd47ccce47a41fb60df8d03a
pureliture <tkdgur1756@naver.com>
2023-07-17 17:00:20 +0900

    :sparkles: 공통 Response 처리를 위한 ResponseBodyAdvice 신규 개발

---
+++ src/main/java/com/team1415/soobookbackend/common/aop/ControllerResponseAdvice.java
@@ -0,0 +1,34 @@
+package com.team1415.soobookbackend.common.aop;
+
+import com.team1415.soobookbackend.common.response.ApiNoContentCode;
+import com.team1415.soobookbackend.common.response.FailResponse;
+import com.team1415.soobookbackend.common.response.SuccessResponse;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice
+public class ControllerResponseAdvice implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request, ServerHttpResponse response) {
+
+        if (ObjectUtils.isEmpty(body)) {
+            return FailResponse.of(new ApiNoContentCode(), "");
+        } else {
+            return SuccessResponse.of(body);
+        }
+    }
+}
