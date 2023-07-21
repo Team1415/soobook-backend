94bec3a0aa9adedf629538c3915d8081d3099ede
pureli <tkdgur1756@naver.com>
2023-07-21 15:40:03 +0900

    :bug: @ResponseBodyAdvice basePackages 미설정에 따른 Swagger3 문서 관련 오류 수정

--- src/main/java/com/team1415/soobookbackend/common/aop/ControllerResponseAdvice.java
+++ src/main/java/com/team1415/soobookbackend/common/aop/ControllerResponseAdvice.java
@@ -12,7 +12,7 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "com.team1415")
 public class ControllerResponseAdvice implements ResponseBodyAdvice<Object> {
 
     @Override
