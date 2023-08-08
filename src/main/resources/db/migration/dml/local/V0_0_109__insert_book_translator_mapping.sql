a050561cdf2239ce4b8239edbbc6740d422df126
pureliture <tkdgur1756@naver.com>
2023-08-08 18:21:39 +0900

    :card_file_box: crawling 데이터 기반 샘플데이터 추가, DML 수정

---
+++ src/main/resources/db/migration/dml/local/V0_0_109__insert_book_translator_mapping.sql
@@ -0,0 +1,54 @@
+INSERT INTO `soobook-database`.book_translator_mapping (book_id,translator_id) VALUES
+	 (8,1),
+	 (10,2),
+	 (25,3),
+	 (26,4),
+	 (35,5),
+	 (36,6),
+	 (37,7),
+	 (39,8),
+	 (41,9),
+	 (42,10);
+INSERT INTO `soobook-database`.book_translator_mapping (book_id,translator_id) VALUES
+	 (43,11),
+	 (44,12),
+	 (46,13),
+	 (47,14),
+	 (48,15),
+	 (49,16),
+	 (50,17),
+	 (51,18),
+	 (52,19),
+	 (53,20);
+INSERT INTO `soobook-database`.book_translator_mapping (book_id,translator_id) VALUES
+	 (54,21),
+	 (55,22),
+	 (56,23),
+	 (58,24),
+	 (60,25),
+	 (62,26),
+	 (64,27),
+	 (98,28),
+	 (107,29),
+	 (111,30);
+INSERT INTO `soobook-database`.book_translator_mapping (book_id,translator_id) VALUES
+	 (114,31),
+	 (132,32),
+	 (138,33),
+	 (141,34),
+	 (142,35),
+	 (145,36),
+	 (146,37),
+	 (147,38),
+	 (187,39),
+	 (188,40);
+INSERT INTO `soobook-database`.book_translator_mapping (book_id,translator_id) VALUES
+	 (193,41),
+	 (194,42),
+	 (195,43),
+	 (202,44),
+	 (203,45),
+	 (205,46),
+	 (207,47),
+	 (213,48),
+	 (214,49);
