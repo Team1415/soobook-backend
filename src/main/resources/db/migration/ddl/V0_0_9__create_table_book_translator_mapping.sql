9a8cc9a290b3b2782ec8944f6e26c611e7d7f2dd
pureliture <tkdgur1756@naver.com>
2023-08-24 14:47:25 +0900

    :card_file_box: 테이블 인덱스 일괄 생성

--- src/main/resources/db/migration/ddl/V0_0_9__create_table_book_translator_mapping.sql
+++ src/main/resources/db/migration/ddl/V0_0_9__create_table_book_translator_mapping.sql
@@ -2,5 +2,7 @@ CREATE TABLE `book_translator_mapping` (
   id bigint AUTO_INCREMENT PRIMARY KEY COMMENT '역자도서매핑아이디',
   book_id bigint COMMENT '도서아이디',
   translator_id bigint COMMENT '역자아이디',
-  UNIQUE KEY `book_translator_mapping_UN` (`book_id`,`translator_id`)
+  UNIQUE KEY `book_translator_mapping_UN` (`book_id`,`translator_id`),
+  INDEX book_id (book_id),
+  INDEX translator_id (translator_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='도서역자매핑'
