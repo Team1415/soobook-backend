e8114faa2e6216e1265901f72037fdb5da624342
jiho-s <tlswlgh120@naver.com>
2023-08-26 10:40:28 +0900

    :art: format

--- src/main/java/com/team1415/soobookbackend/security/jwt/domain/port/JwtTokenPort.java
+++ src/main/java/com/team1415/soobookbackend/security/jwt/domain/port/JwtTokenPort.java
@@ -1,9 +1,10 @@
 package com.team1415.soobookbackend.security.jwt.domain.port;
 
 import com.team1415.soobookbackend.security.jwt.domain.JwtClaims;
-import io.jsonwebtoken.Jwt;
 
 public interface JwtTokenPort {
+
     String generateToken(JwtClaims jwtClaims);
+
     JwtClaims parse(String token);
 }
