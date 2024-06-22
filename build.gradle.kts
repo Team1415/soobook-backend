4255ebbc58610c2be8ff192864561daa91a2dcf6
pureli <tkdgur1756@naver.com>
2024-06-22 17:55:09 +0900

    :wrench: 도커 없이 완전 로컬 실행을 위한 gradle 설정 변

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,17 @@ plugins {
   java
   id("org.springframework.boot") version "3.1.1"
   id("io.spring.dependency-management") version "1.1.0"
-  id("org.graalvm.buildtools.native") version "0.9.23"
   id("org.asciidoctor.jvm.convert") version "3.3.2"
-  id("org.flywaydb.flyway") version "9.20.0"
+  // id("org.flywaydb.flyway") version "9.20.0"
   id("com.diffplug.spotless") version "6.19.0"
+  id("org.springdoc.openapi-gradle-plugin") version "1.6.0"
 }
 
 group = "com.team1415"
 version = "0.0.1-SNAPSHOT"
+
+val querydslVersion = "5.0.0"
+val jakartaApiVersion = "3.1.0"
 
 java {
   sourceCompatibility = JavaVersion.VERSION_17
@@ -34,22 +37,46 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-validation")
   implementation("org.springframework.boot:spring-boot-starter-web")
   implementation("org.apache.kafka:kafka-streams")
-  implementation("org.flywaydb:flyway-core")
-  implementation("org.flywaydb:flyway-mysql")
-  implementation("org.mapstruct:mapstruct:1.5.5.Final")
+  // implementation("org.flywaydb:flyway-core")
+  // implementation("org.flywaydb:flyway-mysql")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.1.0")
+  implementation("org.apache.commons:commons-csv:1.10.0")
+  implementation("org.apache.commons:commons-collections4:4.4")
+  implementation("commons-io:commons-io:2.13.0")
+  implementation("com.opencsv:opencsv:5.5")
+  implementation("com.github.ozlerhakan:poiji:4.1.1")
+
+  // QueryDSL Implementation
+  implementation("com.querydsl:querydsl-jpa:${querydslVersion}:jakarta")
+  implementation("com.querydsl:querydsl-core:${querydslVersion}")
+  implementation("com.querydsl:querydsl-collections")
+  annotationProcessor("com.querydsl:querydsl-apt:${querydslVersion}:jakarta")
+  annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+  annotationProcessor("jakarta.persistence:jakarta.persistence-api")
+  testImplementation("jakarta.persistence:jakarta.persistence-api")
+  testImplementation("com.querydsl:querydsl-jpa:${querydslVersion}")
+
   compileOnly("org.projectlombok:lombok")
-  developmentOnly("org.springframework.boot:spring-boot-devtools")
-  developmentOnly("org.springframework.boot:spring-boot-docker-compose")
-  runtimeOnly("com.mysql:mysql-connector-j")
+  implementation("org.projectlombok:lombok-mapstruct-binding:0.2.0")
+  implementation("org.mapstruct:mapstruct:1.5.5.Final")
   annotationProcessor("org.projectlombok:lombok")
   annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+
+  annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+  developmentOnly("org.springframework.boot:spring-boot-devtools")
+  runtimeOnly("com.h2database:h2")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
-  testImplementation("org.springframework.boot:spring-boot-testcontainers")
+  testImplementation("org.testcontainers:testcontainers")
+  testImplementation("org.testcontainers:junit-jupiter")
   testImplementation("io.projectreactor:reactor-test")
   testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
-  testImplementation("org.testcontainers:junit-jupiter")
   testImplementation("org.testcontainers:kafka")
-  testImplementation("org.testcontainers:mysql")
+}
+
+tasks.withType<JavaCompile> {
+  options.annotationProcessorPath = configurations.annotationProcessor.get()
+  options.compilerArgs.add("-Amapstruct.defaultComponentModel=spring")
 }
 
 tasks.withType<Test> {
@@ -65,19 +92,41 @@ tasks.asciidoctor {
   dependsOn(tasks.test)
 }
 
-flyway {
-  url = "jdbc:mysql://localhost:3306/soobook-database?useSSL=false&allowPublicKeyRetrieval=true"
-  locations = arrayOf("filesystem:./src/main/resources/flyway/ddl", "filesystem:./src/main/resources/flyway/dml/local")
-  user = "root"
-  password = "verysecret"
-  schemas = arrayOf("soobook-database")
-  sqlMigrationSuffixes = arrayOf(".sql")
-  outOfOrder = true
-  baselineOnMigrate = true
+val querydslDir = "src/main/generated"
+
+sourceSets {
+  getByName("main").java.srcDirs(querydslDir)
+}
+
+tasks.withType<JavaCompile> {
+  options.generatedSourceOutputDirectory.set(file(querydslDir))
+  options.compilerArgs.add("-Amapstruct.defaultComponentModel=spring")
+}
+
+//flyway {
+//  url = "jdbc:mysql://localhost:3306/soobook-database?useSSL=false&allowPublicKeyRetrieval=true"
+//  locations = arrayOf("filesystem:./src/main/resources/flyway/ddl", "filesystem:./src/main/resources/flyway/dml/local")
+//  user = "root"
+//  password = "verysecret"
+//  schemas = arrayOf("soobook-database")
+//  sqlMigrationSuffixes = arrayOf(".sql")
+//  outOfOrder = true
+//  baselineOnMigrate = true
+//}
+
+tasks.named("clean") {
+  doLast {
+    file(querydslDir).deleteRecursively()
+  }
+}
+
+tasks.named("spotlessApply") {
+  dependsOn("compileJava")
 }
 
 spotless {
   java {
+    target("src/**/*.java", "build/generated-sources/**/*.java")
     importOrder("java", "jakarta", "javax", "org", "com", "common", "")
     removeUnusedImports()
 
@@ -101,5 +150,4 @@ spotless {
     trimTrailingWhitespace()
     endWithNewline()
   }
-
 }
